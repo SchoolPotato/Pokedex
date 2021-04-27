0d65065f636c6c907b3713564141778a2fce5930
Alexander Welton <Awelto389@west-mec.local>
2021-04-26 18:20:16 -0700

    Added pokedex screen as well as jquery

---
+++ main.js
@@ -0,0 +1,238 @@
+// I hate this but since I'm not using node I guess I just gotta slap the json in here
+
+let charizard = `{
+    "abilities": [
+      {
+        "name": "blaze"
+      },
+      {
+        "name": "solar-power"
+      }
+    ],
+    "base_experience": 240,
+    "height": 17,
+    "id": 6,
+    "is_default": true,
+    "name": "charizard",
+    "order": 7,
+    "stats": [
+      {
+        "base_stat": 78,
+        "effort": 0,
+        "stat": {
+          "name": "hp"
+        }
+      },
+      {
+        "base_stat": 84,
+        "effort": 0,
+        "stat": {
+          "name": "attack"
+        }
+      },
+      {
+        "base_stat": 78,
+        "effort": 0,
+        "stat": {
+          "name": "defense"
+        }
+      },
+      {
+        "base_stat": 109,
+        "effort": 3,
+        "stat": {
+          "name": "special-attack"
+        }
+      },
+      {
+        "base_stat": 85,
+        "effort": 0,
+        "stat": {
+          "name": "special-defense"
+        }
+      },
+      {
+        "base_stat": 100,
+        "effort": 0,
+        "stat": {
+          "name": "speed"
+        }
+      }
+    ],
+    "types": [
+      {
+        "slot": 1,
+        "type": {
+          "name": "fire"
+        }
+      },
+      {
+        "slot": 2,
+        "type": {
+          "name": "flying"
+        }
+      }
+    ],
+    "weight": 905
+  }`
+
+let charmeleon = `{
+    "abilities": [
+      {
+        "name": "blaze"
+      },
+      {
+        "name": "solar-power"
+      }
+    ],
+    "base_experience": 240,
+    "height": 17,
+    "id": 6,
+    "is_default": true,
+    "name": "charizard",
+    "order": 7,
+    "stats": [
+      {
+        "base_stat": 78,
+        "effort": 0,
+        "stat": {
+          "name": "hp"
+        }
+      },
+      {
+        "base_stat": 84,
+        "effort": 0,
+        "stat": {
+          "name": "attack"
+        }
+      },
+      {
+        "base_stat": 78,
+        "effort": 0,
+        "stat": {
+          "name": "defense"
+        }
+      },
+      {
+        "base_stat": 109,
+        "effort": 3,
+        "stat": {
+          "name": "special-attack"
+        }
+      },
+      {
+        "base_stat": 85,
+        "effort": 0,
+        "stat": {
+          "name": "special-defense"
+        }
+      },
+      {
+        "base_stat": 100,
+        "effort": 0,
+        "stat": {
+          "name": "speed"
+        }
+      }
+    ],
+    "types": [
+      {
+        "slot": 1,
+        "type": {
+          "name": "fire"
+        }
+      },
+      {
+        "slot": 2,
+        "type": {
+          "name": "flying"
+        }
+      }
+    ],
+    "weight": 905
+  }`
+
+let charmander = `{
+    "abilities": [
+      {
+        "name": "blaze"
+      },
+      {
+        "name": "solar-power"
+      }
+    ],
+    "base_experience": 240,
+    "height": 17,
+    "id": 6,
+    "is_default": true,
+    "name": "charizard",
+    "order": 7,
+    "stats": [
+      {
+        "base_stat": 78,
+        "effort": 0,
+        "stat": {
+          "name": "hp"
+        }
+      },
+      {
+        "base_stat": 84,
+        "effort": 0,
+        "stat": {
+          "name": "attack"
+        }
+      },
+      {
+        "base_stat": 78,
+        "effort": 0,
+        "stat": {
+          "name": "defense"
+        }
+      },
+      {
+        "base_stat": 109,
+        "effort": 3,
+        "stat": {
+          "name": "special-attack"
+        }
+      },
+      {
+        "base_stat": 85,
+        "effort": 0,
+        "stat": {
+          "name": "special-defense"
+        }
+      },
+      {
+        "base_stat": 100,
+        "effort": 0,
+        "stat": {
+          "name": "speed"
+        }
+      }
+    ],
+    "types": [
+      {
+        "slot": 1,
+        "type": {
+          "name": "fire"
+        }
+      },
+      {
+        "slot": 2,
+        "type": {
+          "name": "flying"
+        }
+      }
+    ],
+    "weight": 905
+  }`
+
+  charmander = JSON.parse(charmander);
+  charmeleon = JSON.parse(charmeleon);
+  charizard = JSON.parse(charizard);
+
+
+$(function(){
+    $('.pokedex').append('<img src="images/charizard.png">');
+})
