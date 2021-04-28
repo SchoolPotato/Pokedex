6bcc11697fa321e4810ffc1eae714858cead4741
Alexander Welton <Awelto389@west-mec.local>
2021-04-27 18:17:36 -0700

    Added font, began work on generating content from JSON

--- main.js
+++ main.js
@@ -77,162 +77,152 @@ let charizard = `{
   }`
 
 let charmeleon = `{
-    "abilities": [
-      {
-        "name": "blaze"
-      },
-      {
-        "name": "solar-power"
+  "abilities": [
+    {
+      "name": "blaze"
+    },
+    {
+      "name": "solar-power"
+    }
+  ],
+  "base_experience": 142,
+  "height": 11,
+  "id": 5,
+  "is_default": true,
+  "name": "charmeleon",
+  "order": 6,
+  "stats": [
+    {
+      "base_stat": 58,
+      "effort": 0,
+      "stat": {
+        "name": "hp"
       }
-    ],
-    "base_experience": 240,
-    "height": 17,
-    "id": 6,
-    "is_default": true,
-    "name": "charizard",
-    "order": 7,
-    "stats": [
-      {
-        "base_stat": 78,
-        "effort": 0,
-        "stat": {
-          "name": "hp"
-        }
-      },
-      {
-        "base_stat": 84,
-        "effort": 0,
-        "stat": {
-          "name": "attack"
-        }
-      },
-      {
-        "base_stat": 78,
-        "effort": 0,
-        "stat": {
-          "name": "defense"
-        }
-      },
-      {
-        "base_stat": 109,
-        "effort": 3,
-        "stat": {
-          "name": "special-attack"
-        }
-      },
-      {
-        "base_stat": 85,
-        "effort": 0,
-        "stat": {
-          "name": "special-defense"
-        }
-      },
-      {
-        "base_stat": 100,
-        "effort": 0,
-        "stat": {
-          "name": "speed"
-        }
+    },
+    {
+      "base_stat": 64,
+      "effort": 0,
+      "stat": {
+        "name": "attack"
       }
-    ],
-    "types": [
-      {
-        "slot": 1,
-        "type": {
-          "name": "fire"
-        }
-      },
-      {
-        "slot": 2,
-        "type": {
-          "name": "flying"
-        }
+    },
+    {
+      "base_stat": 58,
+      "effort": 0,
+      "stat": {
+        "name": "defense"
       }
-    ],
-    "weight": 905
-  }`
+    },
+    {
+      "base_stat": 80,
+      "effort": 1,
+      "stat": {
+        "name": "special-attack"
+      }
+    },
+    {
+      "base_stat": 65,
+      "effort": 0,
+      "stat": {
+        "name": "special-defense"
+      }
+    },
+    {
+      "base_stat": 80,
+      "effort": 1,
+      "stat": {
+        "name": "speed"
+      }
+    }
+  ],
+  "types": [
+    {
+      "slot": 1,
+      "type": {
+        "name": "fire"
+      }
+    }
+  ],
+  "weight": 190
+}`
 
 let charmander = `{
-    "abilities": [
-      {
-        "name": "blaze"
-      },
-      {
-        "name": "solar-power"
+  "abilities": [
+    {
+      "name": "blaze"
+    },
+    {
+      "name": "solar-power"
+    }
+  ],
+  "base_experience": 62,
+  "height": 6,
+  "id": 4,
+  "is_default": true,
+  "name": "charmander",
+  "order": 5,
+  "stats": [
+    {
+      "base_stat": 39,
+      "effort": 0,
+      "stat": {
+        "name": "hp"
       }
-    ],
-    "base_experience": 240,
-    "height": 17,
-    "id": 6,
-    "is_default": true,
-    "name": "charizard",
-    "order": 7,
-    "stats": [
-      {
-        "base_stat": 78,
-        "effort": 0,
-        "stat": {
-          "name": "hp"
-        }
-      },
-      {
-        "base_stat": 84,
-        "effort": 0,
-        "stat": {
-          "name": "attack"
-        }
-      },
-      {
-        "base_stat": 78,
-        "effort": 0,
-        "stat": {
-          "name": "defense"
-        }
-      },
-      {
-        "base_stat": 109,
-        "effort": 3,
-        "stat": {
-          "name": "special-attack"
-        }
-      },
-      {
-        "base_stat": 85,
-        "effort": 0,
-        "stat": {
-          "name": "special-defense"
-        }
-      },
-      {
-        "base_stat": 100,
-        "effort": 0,
-        "stat": {
-          "name": "speed"
-        }
+    },
+    {
+      "base_stat": 52,
+      "effort": 0,
+      "stat": {
+        "name": "attack"
       }
-    ],
-    "types": [
-      {
-        "slot": 1,
-        "type": {
-          "name": "fire"
-        }
-      },
-      {
-        "slot": 2,
-        "type": {
-          "name": "flying"
-        }
+    },
+    {
+      "base_stat": 43,
+      "effort": 0,
+      "stat": {
+        "name": "defense"
       }
-    ],
-    "weight": 905
-  }`
+    },
+    {
+      "base_stat": 60,
+      "effort": 0,
+      "stat": {
+        "name": "special-attack"
+      }
+    },
+    {
+      "base_stat": 50,
+      "effort": 0,
+      "stat": {
+        "name": "special-defense"
+      }
+    },
+    {
+      "base_stat": 65,
+      "effort": 1,
+      "stat": {
+        "name": "speed"
+      }
+    }
+  ],
+  "types": [
+    {
+      "slot": 1,
+      "type": {
+        "name": "fire"
+      }
+    }
+  ],
+  "weight": 85
+}`
 
-  charmander = JSON.parse(charmander);
-  charmeleon = JSON.parse(charmeleon);
-  charizard = JSON.parse(charizard);
+charmander = JSON.parse(charmander);
+charmeleon = JSON.parse(charmeleon);
+charizard = JSON.parse(charizard);
 
 
 $(function(){
-    $('.pokedex').append('<img src="images/charizard.png">');
+  $('.pokedex').append(`<img src="images/${charmander.name}.png">`);
+  $('.info').append(`<p class=stats>Name: ${charmander.name}<br>Base XP: ${charmander.base_experience}<br>Height: ${charmander.height}<br>Weight: ${charmander.weight}</p>`);
+  $('.info').append(`<p class="ablities">Abilities: ${charmander.abilities[0].name}, ${charmander.abilities[1].name}</p>`);
 })
